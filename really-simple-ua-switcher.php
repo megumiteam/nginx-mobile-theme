db349554798689f97750c4578a3f05103a501607
miya <miya@wpist.me>
2013-11-05 17:32:33 +0900

    some update

--- really-simple-ua-switcher.php
+++ really-simple-ua-switcher.php
@@ -26,12 +26,60 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 require(dirname(__FILE__).'/vendor/autoload.php');
 
-$ua = $_SERVER['HTTP_USER_AGENT'];
-$pattern = '/(iPhone|iPod|incognito|webmate|Android|dream|CUPCAKE|froyo|BlackBerry|webOS|s8000|bada|IEMobile|Googlebot\-Mobile|AdsBot\-Google)/i';
-$pattern = apply_filters( 'rsus_regular_expression', $pattern );
-$switch_theme = apply_filters( 'rsus_switch_theme_name','' );
+$rsua = new Really_Simple_UA_Switcher();
+$rsua->init();
 
-if ( !empty($pattern) && !empty($switch_theme) && isset($ua) && preg_match( $pattern, $ua ) ) {
-    $theme = new Megumi_SwitchTheme($switch_theme);
+class Really_Simple_UA_Switcher {
+
+function __construct()
+{
+}
+
+public function init()
+{
+    add_action('plugins_loaded', array($this, 'plugins_loaded'), 9999);
+}
+
+public function plugins_loaded()
+{
+    $mobile_detect = $this->mobile_detect();
+    if ($mobile_detect) {
+        $mobile_theme = get_option("rsus_mobile_theme", wp_get_theme());
+        $mobile_theme = apply_filters(
+            'rsus_mobile_theme-'.$mobile_detect,
+            $mobile_theme
+        );
+        $this->switch_theme($mobile_theme);
+
+        add_filter('nginxchampuru_db_cached_url', 'nginxchampuru_db_cached_url');
+    }
+}
+
+public function nginxchampuru_db_cached_url($url)
+{
+    $mobile_detect = $this->mobile_detect();
+    return $url.$mobile_detect;
+}
+
+private function switch_theme($theme)
+{
+    $theme = new Megumi_SwitchTheme($theme);
     $theme->apply();
 }
+
+private function mobile_detect()
+{
+    $mobile_detect = '';
+
+    if (isset($_SERVER['X-UA-Detect']) && $_SERVER['X-UA-Detect']) {
+        // @ktai ore @smartphone or @smartphone.off on Amimoto
+        $mobile_detect = $_SERVER['X-UA-Detect'];
+    } elseif (function_exists('wp_is_mobile') && wp_is_mobile()) {
+        $mobile_detect = '@smartphone';
+    }
+
+    return apply_filters("rsus_mobile_detect", $mobile_detect);
+}
+
+}
+
