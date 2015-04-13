096767be66182c0d7ebbc1a2bb1faf8f0d82c0b7
miya0001 <miya@wpist.me>
2015-04-13 16:34:57 +0900

    compatibles to coding standard

--- nginx-mobile-theme.php
+++ nginx-mobile-theme.php
@@ -7,12 +7,12 @@ Author: miyauchi, megumithemes
 Version: 1.6.0
 Author URI: http://ninjax.cc/
 
-Copyright 2013 Ninjax Team (email : info@ninjax.cc)
+Copyright 2013 Ninjax Team ( email : info@ninjax.cc )
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
+( at your option ) any later version.
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -24,19 +24,19 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-require(dirname(__FILE__).'/vendor/autoload.php');
+require( dirname( __FILE__ ).'/vendor/autoload.php' );
 
 $nginx_mobile_theme = new Nginx_Mobile_Theme();
 $nginx_mobile_theme->init();
 
 class Nginx_Mobile_Theme{
 
-private $mobile_detects = array('@smartphone');
+private $mobile_detects = array( '@smartphone' );
 private $nginxcc = 'nginx-champuru/nginx-champuru.php';
 
 public function init()
 {
-	add_action('plugins_loaded', array($this, 'plugins_loaded'), 9999);
+	add_action( 'plugins_loaded', array( $this, 'plugins_loaded' ), 9999 );
 }
 
 /**
@@ -47,46 +47,46 @@ public function init()
  */
 public function plugins_loaded()
 {
-	if (is_admin()) {
-		add_action('admin_init', array($this, 'admin_init'));
+	if ( is_admin() ) {
+		add_action( 'admin_init', array( $this, 'admin_init' ) );
 		add_action(
 			'customize_controls_print_scripts',
-			array($this, 'customize_controls_print_scripts'),
+			array( $this, 'customize_controls_print_scripts' ),
 			9999
 		);
 	}
 
-	if (defined('IS_AMIMOTO') && IS_AMIMOTO === true) {
+	if ( defined( 'IS_AMIMOTO' ) && IS_AMIMOTO === true ) {
 		$this->amimoto_support(); // see http://megumi-cloud.com/
 	}
 
-	if (!has_filter('nginxmobile_mobile_themes')) {
-		add_action('customize_register', array($this, 'customize_register'));
+	if ( !has_filter( 'nginxmobile_mobile_themes' ) ) {
+		add_action( 'customize_register', array( $this, 'customize_register' ) );
 	}
 
 	$mobile_detect = $this->mobile_detect();
-	if ($mobile_detect) {
-		$mobile_theme = get_option("nginxmobile_mobile_themes");
+	if ( $mobile_detect ) {
+		$mobile_theme = get_option( "nginxmobile_mobile_themes" );
 		/**
 		 * Filter the theme slug for mobile
 		 *
 		 * @since 1.0.0
 		 * @param string $mobile_theme theme slug
 		 */
-		$mobile_theme = apply_filters('nginxmobile_mobile_themes', $mobile_theme);
-		$detect = str_replace('@', '', $mobile_detect);
-		if (isset($mobile_theme[$detect]) && $mobile_theme[$detect]) {
-			$this->switch_theme($mobile_theme[$detect]);
+		$mobile_theme = apply_filters( 'nginxmobile_mobile_themes', $mobile_theme );
+		$detect = str_replace( '@', '', $mobile_detect );
+		if ( isset( $mobile_theme[$detect] ) && $mobile_theme[$detect] ) {
+			$this->switch_theme( $mobile_theme[$detect] );
 		}
 		add_filter(
 			'nginxchampuru_get_the_url',
-			array($this, 'nginxchampuru_get_the_url')
+			array( $this, 'nginxchampuru_get_the_url' )
 		);
-	} elseif (is_user_logged_in()) { // theme preview
-		if (isset($_GET['nginx-mobile-theme']) && $_GET['nginx-mobile-theme']) {
-			if (preg_match('/^[a-zA-Z0-9\-]+$/', $_GET['nginx-mobile-theme'])) {
-				$this->switch_theme($_GET['nginx-mobile-theme']);
-				add_filter('home_url', array($this, 'home_url'));
+	} elseif ( is_user_logged_in() ) { // theme preview
+		if ( isset( $_GET['nginx-mobile-theme'] ) && $_GET['nginx-mobile-theme'] ) {
+			if ( preg_match( '/^[a-zA-Z0-9\-]+$/', $_GET['nginx-mobile-theme'] ) ) {
+				$this->switch_theme( $_GET['nginx-mobile-theme'] );
+				add_filter( 'home_url', array( $this, 'home_url' ) );
 			}
 		}
 	}
@@ -98,11 +98,11 @@ public function plugins_loaded()
  * @access public
  * @since  1.2.0
  */
-public function home_url($url)
+public function home_url( $url )
 {
-	if (is_user_logged_in()) { // theme preview
-		if (isset($_GET['nginx-mobile-theme']) && $_GET['nginx-mobile-theme']) {
-			if (preg_match('/^[a-zA-Z0-9\-]+$/', $_GET['nginx-mobile-theme'])) {
+	if ( is_user_logged_in() ) { // theme preview
+		if ( isset( $_GET['nginx-mobile-theme'] ) && $_GET['nginx-mobile-theme'] ) {
+			if ( preg_match( '/^[a-zA-Z0-9\-]+$/', $_GET['nginx-mobile-theme'] ) ) {
 				return add_query_arg(
 					array(
 						'nginx-mobile-theme' => $_GET['nginx-mobile-theme']
@@ -125,13 +125,13 @@ public function customize_controls_print_scripts()
 {
 ?>
 <script type="text/javascript">
-jQuery(document).ready(function(){
+jQuery( document ).ready( function(){
 	var $ = jQuery;
-	$('.theme-preview').click(function(){
-		var theme = $('select:first', $(this).parent().parent()).val();
-		window.open().location.href = '<?php echo home_url('/'); ?>?nginx-mobile-theme='+theme;
-	});
-});
+	$( '.theme-preview' ).click( function(){
+		var theme = $( 'select:first', $( this ).parent().parent() ).val();
+		window.open().location.href = '<?php echo home_url( '/' ); ?>?nginx-mobile-theme='+theme;
+	} );
+} );
 </script>
 <?php
 }
@@ -144,8 +144,8 @@ jQuery(document).ready(function(){
  */
 public function admin_init()
 {
-	if (function_exists('is_plugin_inactive') && is_plugin_inactive($this->nginxcc)) {
-		add_action('admin_notices', array($this, 'admin_notice'));
+	if ( function_exists( 'is_plugin_inactive' ) && is_plugin_inactive( $this->nginxcc ) ) {
+		add_action( 'admin_notices', array( $this, 'admin_notice' ) );
 	}
 }
 
@@ -157,7 +157,7 @@ public function admin_init()
  */
 public function admin_notice()
 {
-	$install_url = admin_url('plugin-install.php?tab=search&s=nginx-champuru&plugin-search-input=Search+Plugins');
+	$install_url = admin_url( 'plugin-install.php?tab=search&s=nginx-champuru&plugin-search-input=Search+Plugins' );
 	?>
 	<div class="error">
 		<p>Nginx Mobile Theme requires <strong>Nginx Cache Controller</strong>.
@@ -172,39 +172,39 @@ public function admin_notice()
  * @access public
  * @since  1.0.0
  */
-public function customize_register($wp_customize)
+public function customize_register( $wp_customize )
 {
 	$all_themes = wp_get_themes();
 	$themes = array();
-	foreach ($all_themes as $theme_name => $theme) {
-		$themes[$theme_name] = $theme->get('Name');
+	foreach ( $all_themes as $theme_name => $theme ) {
+		$themes[$theme_name] = $theme->get( 'Name' );
 	}
 
-	$wp_customize->add_section('nginxmobile', array(
+	$wp_customize->add_section( 'nginxmobile', array(
 		'title'		  => 'Mobile Theme',
 		'priority'	   => 9999,
-	));
+	) );
 
-	foreach ($this->get_mobile_detects() as $detect) {
-		$detect = esc_html(str_replace('@', '', $detect));
+	foreach ( $this->get_mobile_detects() as $detect ) {
+		$detect = esc_html( str_replace( '@', '', $detect ) );
 		$current_theme = wp_get_theme();
-		$wp_customize->add_setting('nginxmobile_mobile_themes['.$detect.']', array(
+		$wp_customize->add_setting( 'nginxmobile_mobile_themes['.$detect.']', array(
 			'default'		=> $current_theme->get_stylesheet(),
 			'type'		   => 'option',
 			'capability'	 => 'switch_themes',
-		));
+		) );
 
-		if ($detect === 'ktai') { // amimoto fix
-			if (defined('WP_LANG') && WP_LANG === 'ja') {
-				$label = ucfirst($detect).' theme';
+		if ( $detect === 'ktai' ) { // amimoto fix
+			if ( defined( 'WP_LANG' ) && WP_LANG === 'ja' ) {
+				$label = ucfirst( $detect ).' theme';
 			} else {
 				$label = 'Cell-Phone theme';
 			}
 		} else {
-			$label = ucfirst($detect).' theme';
+			$label = ucfirst( $detect ).' theme';
 		}
 
-		$wp_customize->add_control(new Megumi_ThemeCustomizerControl(
+		$wp_customize->add_control( new Megumi_ThemeCustomizerControl(
 			$wp_customize,
 			'nginxmobile_mobile_themes-'.$detect,
 			array(
@@ -213,9 +213,9 @@ public function customize_register($wp_customize)
 				'section'	 => 'nginxmobile',
 				'type'		=> 'select',
 				'choices'	 => $themes,
-				'label_after' => '<a href="javascript:void(0);" class="theme-preview">Theme Preview</a>',
+				'label_after' => '<a href="javascript:void( 0 );" class="theme-preview">Theme Preview</a>',
 			)
-		));
+		) );
 	}
 }
 
@@ -225,7 +225,7 @@ public function customize_register($wp_customize)
  * @access public
  * @since  1.0.0
  */
-public function nginxchampuru_get_the_url($url)
+public function nginxchampuru_get_the_url( $url )
 {
 	$mobile_detect = $this->mobile_detect();
 	return sprintf(
@@ -234,12 +234,12 @@ public function nginxchampuru_get_the_url($url)
 		 *
 		 * @since 1.0.0
 		 * @param string $proxy_key An string for proxy key.
-		 * @param string $url	   An original URL.
+		 * @param string $url       An original URL.
 		 */
 		apply_filters(
 			"nginxmobile_proxy_key",
-			'%s'.str_replace('%', '%%', $url),
-			str_replace('%', '%%', $url)
+			'%s'.str_replace( '%', '%%', $url ),
+			str_replace( '%', '%%', $url )
 		),
 		$mobile_detect
 	);
@@ -259,7 +259,7 @@ private function get_mobile_detects()
 	 * @since 1.0.0
 	 * @param array $mobile_detects An array of determined result of user agent
 	 */
-	return apply_filters("nginxmobile_mobile_detects", $this->mobile_detects);
+	return apply_filters( "nginxmobile_mobile_detects", $this->mobile_detects );
 }
 
 /**
@@ -268,9 +268,9 @@ private function get_mobile_detects()
  * @access private
  * @since  1.0.0
  */
-private function switch_theme($theme)
+private function switch_theme( $theme )
 {
-	$switch_theme = new Megumi_SwitchTheme($theme);
+	$switch_theme = new Megumi_SwitchTheme( $theme );
 	$switch_theme->apply();
 }
 
@@ -284,7 +284,7 @@ public function mobile_detect()
 {
 	$mobile_detect = '';
 
-	if (isset($_SERVER['HTTP_X_UA_DETECT']) && $_SERVER['HTTP_X_UA_DETECT']) {
+	if ( isset( $_SERVER['HTTP_X_UA_DETECT'] ) && $_SERVER['HTTP_X_UA_DETECT'] ) {
 		$mobile_detect = $_SERVER['HTTP_X_UA_DETECT'];
 	}
 
@@ -294,7 +294,7 @@ public function mobile_detect()
 	 * @since 1.0.0
 	 * @param string $mobile_detect  e.g. "@smartphone"
 	 */
-	return apply_filters("nginxmobile_mobile_detect", $mobile_detect);
+	return apply_filters( "nginxmobile_mobile_detect", $mobile_detect );
 }
 
 /**
@@ -305,13 +305,13 @@ public function mobile_detect()
  */
 private function amimoto_support()
 {
-	if (defined('IS_AMIMOTO') && IS_AMIMOTO === true) {
-		add_filter('nginxmobile_proxy_key', function($key, $url){
+	if ( defined( 'IS_AMIMOTO' ) && IS_AMIMOTO === true ) {
+		add_filter( 'nginxmobile_proxy_key', function( $key, $url ){
 			return $url.'%s';
-		}, 10, 2);
-		add_filter('nginxmobile_mobile_detects', function(){
-			return array('@ktai', '@smartphone');
-		});
+		}, 10, 2 );
+		add_filter( 'nginxmobile_mobile_detects', function(){
+			return array( '@ktai', '@smartphone' );
+		} );
 	}
 }
 
